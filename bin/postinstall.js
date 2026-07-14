ad3965ae692d44156e4fdc651553a0638a99a1fc
shane00413-wq <shane.00413@gmail.com>
2026-07-14 18:12:14 +0000

    update

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
+
 import fs from "fs";
 import path from "path";
-const pkg=path.join(process.cwd(),"package.json");
-if(fs.existsSync(pkg)){
- const d=JSON.parse(fs.readFileSync(pkg,"utf8"));
- d.scripts ??= {};
- if(!d.scripts["new-doc"]) d.scripts["new-doc"]="new-doc";
- fs.writeFileSync(pkg,JSON.stringify(d,null,2)+"\n");
+
+const pkg = path.join(process.cwd(), "package.json");
+
+if (!fs.existsSync(pkg)) {
+  process.exit(0);
+}
+
+try {
+  const d = JSON.parse(fs.readFileSync(pkg, "utf8"));
+
+  d.scripts ??= {};
+
+  if (!d.scripts["new-doc"]) {
+    d.scripts["new-doc"] = "new-doc";
+    fs.writeFileSync(pkg, JSON.stringify(d, null, 2) + "\n");
+  }
+} catch {
+  process.exit(0);
 }
